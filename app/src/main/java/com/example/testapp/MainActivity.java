5a185256cc14c476566631fc137bd8d3b39f78c7
yaniv1888 <34810001+yaniv1888@users.noreply.github.com>
2021-04-25 16:15:46 +0300

    UpdateFiles

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Customersbutton = (Button) findViewById(R.id.button1);
         Customersbutton.setOnClickListener(new View.OnClickListener() {
